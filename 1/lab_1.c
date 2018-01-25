303fbd11c4d4f2cb6a5828c35d2ac4b8f2c7d278
Dominik Harmim <harmim6@gmail.com>
2018-01-25 12:53:32 +0000

    Lab 1

---
+++ 1/lab_1.c
@@ -0,0 +1,28 @@
+/**
+ * @author Dominik Harmim <harmim6@gmail.com>
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void exercise1()
+{
+	printf("\nHallo World!\n");
+}
+
+
+void exercise2()
+{
+	printf("\nHallo I'm Exercise 2!\n");
+}
+
+
+int main()
+{
+	exercise1();
+	exercise2();
+
+	return EXIT_SUCCESS;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ICP-1029: Imperative Programming in C - Labs
+
+#### Author: Dominik Harmim <harmim6@gmail.com>
